8ea9c395f7bc0a259b59d5548919f54e0174ddca
Anthony <anthonymwakulila@gmail.com>
2024-07-14 01:09:49 +0300

    added POST,GET,PUT,PATCH,GET

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,13 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response #when call api view return res
+from rest_framework import status #list of of handy http  EG. BAD HTTP_400_BAD_REQUEST
+from profiles_api import serializers #FILE created in profiles api
 
 
 # Create your views here.
+
 #How it works
 #define url which is endpoint then assign it to
 #views then django returns correct res
 class HelloApiView(APIView):#class based on APIView
     """test api view"""
+    serializer_class = serializers.HelloSerializer #ACCESSED OUR SERIALIZER class
+
     #accept get request
     def get(self, request, format=None):
         """return a list of apiview features"""
@@ -20,3 +25,29 @@ class HelloApiView(APIView):#class based on APIView
         ]
 
         return Response({'our_message':'Hello ', 'an_apiview':an_apiview})#contain a dictionary or list convert res to json = list or dict
+
+    def post(self, request):
+        """post method to return hello with passed name that is serialized"""
+        serializer = self.serializer_class(data=request.data)#PASSED ARGUMENTS OF HelloSerializer CLASS - CHECKS ALL VALIDATES RULESS IN THE SERIALIZER CLASS
+
+        if serializer.is_valid():
+            returned_name = serializer.validated_data.get('name')
+            returned_username = serializer.validated_data.get('username')
+            message = f'Welcome to programming {returned_name}'
+            message2 = f'validated username {returned_username}'
+            return Response({ 'our_message':message,'our_message2':message2 })
+        else:
+            return Response(serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+    def put(self, request, pk=None):
+        """hanle updating an object - REPLACING"""
+        return Response({ 'method':'PUT' })
+
+    def patch(self, request, pk=None):
+        """handle partial update UPDATING"""
+        return Response({ 'method':'PATCH' })
+
+    def delete(self, request,pk=None):
+        """delete an object permanentky"""
+        return Response({ 'method':'DELETE' })
